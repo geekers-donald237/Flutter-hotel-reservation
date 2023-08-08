d6d5ac5882325ff63d9485938497a6450bdf8626
Achaire_Zogo <61749305+Achaire-Zogo@users.noreply.github.com>
2023-08-08 21:16:28 +0100

    ok

--- lib/urls/all_url.dart
+++ lib/urls/all_url.dart
@@ -5,5 +5,5 @@ class Urls {
   // static const String baseURL = "http://rental.aangaraa-eat.com/api";
   static const String baseURL = "http://192.168.88.3:8000/api";
 
-  static const String user = baseURL + "/rentali_user";
+  static const String user = "$baseURL/rentali_user";
 }
